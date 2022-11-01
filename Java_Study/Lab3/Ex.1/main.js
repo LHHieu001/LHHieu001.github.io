4be618c577cb330031f30cf05088a9c562ed7dcc
LHHieu001 <115911736+LHHieu001@users.noreply.github.com>
2022-11-01 13:48:44 +0700

    Add files via upload

---
+++ Java_Study/Lab3/Ex.1/main.js
@@ -0,0 +1,8 @@
+/* This is the first JavaScript code that I have written
+Written by: Le Hoang Hieu
+Written on: 01/11/2022
+*/
+
+document.write('Good Afternoon Le Hoang Hieu');
+document.write('<h1>Would you like some cupcakes?</h1>');
+//The end of my first program
