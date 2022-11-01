06c77c86ef35b7ee0406942761dadfa5080940c2
LHHieu001 <115911736+LHHieu001@users.noreply.github.com>
2022-11-01 14:26:32 +0700

    Add files via upload

---
+++ Java_Study/Lab3/Ex2/main.js
@@ -0,0 +1,16 @@
+function calcCost() {
+    //This function will calculate the cost of cupcakes based on what the number the user
+    //enters in the text box
+    //Variable Declaration
+     var costPerCupcake;
+     var amountOfCupcakes;
+     var totalCost;
+    //do calculations
+     costPerCupcake=3;
+     amountOfCupcakes =document.getElementById("amtToGet").value;
+     totalCost = amountOfCupcakes * costPerCupcake;
+    //display answers
+     document.write("<p><h1>Total Cost: ");
+     document.write(totalCost);
+     document.write(" dollars </h1></p>");
+    } //end of function
