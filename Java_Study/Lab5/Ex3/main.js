bf239776822cf2613a2d7e1d53b5f45c153f8b2e
Hieu Le Hoang (Goatee) <115911736+LHHieu001@users.noreply.github.com>
2022-11-28 21:39:56 +0700

    Add files via upload

---
+++ Java_Study/Lab5/Ex3/main.js
@@ -0,0 +1,13 @@
+function testing123() {
+    if (1 <= 2) {
+    alert ("Whew 1 is still less than 2");
+    alert ("i am still inside the true part of the if");
+    } 
+    else {
+    alert ("Oh no, this is false");
+    alert ("I am still inside the if statement, but now in the false part of it");
+    } 
+    alert ("I am outside of the if");
+    alert ("at the end of testing123");
+   } 
+   
